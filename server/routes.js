9443fa3b1e846c66ca92887332b40abd032f4af9
li8 <srivastava.suhas@gmail.com>
2017-12-31 03:39:22 +0530

    Update docs

--- server/routes.js
+++ server/routes.js
@@ -7,34 +7,20 @@ module.exports = app => {
 
 
   /**
-   * @api {post} /users Register a new user
-   * @apiGroup User
-   * @apiParam {String} name User name
-   * @apiParam {String} email User email
-   * @apiParam {String} password User password
+   * @api {post} /upload
+   * @apiGroup Upload
+   * @apiVersion 1.0.0
+   * @apiParam {String} name DataSet Name
    * @apiParamExample {json} Input
    *    {
-   *      "name": "John Connor",
-   *      "email": "john@connor.net",
-   *      "password": "123456"
+   *      "name": "set#1",
    *    }
-   * @apiSuccess {Number} id User id
-   * @apiSuccess {String} name User name
-   * @apiSuccess {String} email User email
-   * @apiSuccess {String} password User encrypted password
-   * @apiSuccess {Date} updated_at Update's date
-   * @apiSuccess {Date} created_at Register's date
    * @apiSuccessExample {json} Success
    *    HTTP/1.1 200 OK
    *    {
-   *      "id": 1,
-   *      "name": "John Connor",
-   *      "email": "john@connor.net",
-   *      "password": "$2a$10$SK1B1",
-   *      "updated_at": "2016-02-10T15:20:11.700Z",
-   *      "created_at": "2016-02-10T15:29:11.700Z",
+   *      "msg": "set#1 dataset added. Scheduled to generate experiments.",
    *    }
-   * @apiErrorExample {json} Register error
+   * @apiErrorExample {json} Error
    *    HTTP/1.1 412 Precondition Failed
    */
 
@@ -46,7 +32,7 @@ module.exports = app => {
 
     var name = helper.getName(req);
     if(!name){
-      res.status(412).json({msg:"Name parameters missing"});
+      res.status(412).json({msg:"Precondition Failed"});
       return;
     }
     DataSets.create(req.body)
@@ -59,6 +45,28 @@ module.exports = app => {
       });
 
   })
+
+
+
+    /**
+     * @api {put} /upload
+     * @apiGroup Upload
+     * @apiVersion 1.0.0
+     * @apiParam {String} name DataSet Name
+     * @apiParamExample {json} Input
+     *    {
+     *      "name": "set#1",
+     *    }
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "msg": "set#1 dataset added. Scheduled to generate experiments.",
+     *    }
+     * @apiErrorExample {json} Error
+     *    HTTP/1.1 412 Precondition Failed
+     */
+
+
 
 
   app.put('/upload', helper.upload.array('dataset', fileLimit), function (req, res) {
@@ -87,17 +95,56 @@ module.exports = app => {
 
 
 
-  // train --- /id {params: i[],j[],k[]} --< scheduled_id & check state
-  // test -- /id {img} -- < results
+
+    /**
+     * @api {post} /train
+     * @apiGroup Train
+     * @apiVersion 1.0.0
+     * @apiParam {String} name DataSet Name
+     * @apiParamExample {json} Input
+     *    {
+     *      "name": "set#1",
+     *      "i": [0.001,0.01,0.1],
+     *      "j": [1,2,4],
+     *      "k": [1000,2000,4000]
+     *    }
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      "msg": "set#1 dataset added. Scheduled to generate experiments.",
+     *    }
+     * @apiErrorExample {json} Error
+     *    HTTP/1.1 412 {
+     *      "msg": "Precondition Failed"
+     *    }
+     */
+
+
+
 
   app.post('/train',function (req, res) {
     // get dataset -- generate more experiments with params
     // return scheduled_id & check state
     var name = helper.getName(req);
     if(!name){
-      res.status(412).json({msg:"Name parameters missing"});
+      res.status(412).json({msg:"Precondition Failed: name"});
       return;
     }
+
+    if(!req.body.i || req.body.i.length === 0){
+      res.status(412).json({msg:"Precondition Failed: i value"});
+      return;
+    }
+    if(!req.body.j || req.body.j.length === 0){
+      res.status(412).json({msg:"Precondition Failed: j value"});
+      return;
+    }
+    if(!req.body.k || req.body.k.length === 0){
+      res.status(412).json({msg:"Precondition Failed: k value"});
+      return;
+    }
+
+
     var cb = (error,dataset)=>{
       if(error){
         res.status(412).json({ msg: error.message });
@@ -120,18 +167,40 @@ module.exports = app => {
   })
 
 
-// response :
-//{
-//   'i': '0.001',
-//   'k': '2000',
-//   'j': '2',
-//   'image': '/home/li8/Pictures/Screenshot_20171219_164923.png',
-//   'accuracy': 0.8825040064299458
-// }
+
+
+    /**
+     * @api {post} /test
+     * @apiGroup Test
+     * @apiVersion 1.0.0
+     * @apiParam {String} name DataSet Name
+     * @apiParamExample {json} Input
+     *    {
+     *      "name": "set#1"
+     *    }
+     * @apiSuccess {Decimal} i Experiment i
+     * @apiSuccess {Decimal} j Experiment j
+     * @apiSuccess {Decimal} k Experiment k
+     * @apiSuccess {Decimal} accuracy Experiment accuracy
+     * @apiSuccessExample {json} Success
+     *    HTTP/1.1 200 OK
+     *    {
+     *      'i': '0.001',
+     *      'k': '2000',
+     *      'j': '2',
+     *      'accuracy': 0.8825040064299458
+     *    }
+     * @apiErrorExample {json} Error
+     *    HTTP/1.1 412 {
+     *      "msg": "Precondition Failed"
+     *    }
+     */
 
 
 
-  app.post('/test',helper.upload.single(),function(req,res){
+
+
+  app.post('/test',helper.uploadTest.single(),function(req,res){
     var name = helper.getName(req);
     if(!name){
       res.status(412).json({msg:"Name parameters missing"});
@@ -176,7 +245,7 @@ module.exports = app => {
           			console.log("Tested for " + name + " : " + text);
                 var rslt = JSON.parse(text);
                 delete rslt.image;
-                res.status(200).json({result: rslt.img});
+                res.status(200).json({result: rslt});
           		});
           }else{
             res.status(412).json({ msg: "No experiment - found " });
